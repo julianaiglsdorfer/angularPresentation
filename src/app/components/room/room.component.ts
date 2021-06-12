450102c7f1e31e64b69fa334d1b18823b1ef7c6c
Deathacus <34517761+Deathacus@users.noreply.github.com>
2021-06-12 19:45:04 +0200

    fixes, styling,...

--- src/app/components/room/room.component.ts
+++ src/app/components/room/room.component.ts
@@ -7,6 +7,7 @@ import { RoomApiService } from '../../service/room-api.service';
   templateUrl: './room.component.html',
   styleUrls: ['./room.component.css']
 })
+
 export class RoomComponent implements OnInit {
 
   rooms: Room[] = [];
@@ -25,22 +26,22 @@ export class RoomComponent implements OnInit {
     console.log('freeRooms');
     console.log(this.searchModel);
 
-    if(this.searchModel.checkindate && this.searchModel.checkoutdate){//trusy request means not null && not undefined && not 0
-      let fromDate: Date = new Date(Number(this.searchModel.checkindate.substr(0,4)), Number(this.searchModel.checkindate.substr(4,2)), Number(this.searchModel.checkindate.substr(6,2)));
-      let toDate: Date = new Date(Number(this.searchModel.checkoutdate.substr(0,4)), Number(this.searchModel.checkoutdate.substr(4,2)), Number(this.searchModel.checkoutdate.substr(6,2)))
 
-      if(fromDate < toDate){
-        console.log("From Tag: " + fromDate.getDay());
-        console.log("From Monat: " + fromDate.getMonth());
-        console.log("From Jahr: " + fromDate.getFullYear());
-        console.log("To Tag: " + fromDate.getDay());
-        console.log("To Monat: " + fromDate.getMonth());
-        console.log("To Jahr: " + fromDate.getFullYear());
-      }
+    let fromDate: Date = new Date(Number(this.searchModel.checkindate.substr(0,4)), Number(this.searchModel.checkindate.substr(4,2)), Number(this.searchModel.checkindate.substr(6,2)));
+    let toDate: Date = new Date(Number(this.searchModel.checkoutdate.substr(0,4)), Number(this.searchModel.checkoutdate.substr(4,2)), Number(this.searchModel.checkoutdate.substr(6,2)))
+
+    if(fromDate < toDate){
+      console.log("From Tag: " + fromDate.getDay());
+      console.log("From Monat: " + fromDate.getMonth());
+      console.log("From Jahr: " + fromDate.getFullYear());
+      console.log("To Tag: " + fromDate.getDay());
+      console.log("To Monat: " + fromDate.getMonth());
+      console.log("To Jahr: " + fromDate.getFullYear());
       this.roomApiService.getFreeRooms(this.searchModel).subscribe(
-      response => this.rooms = response,
-    );
+        response => this.rooms = response,
+      );
     }
+
     else{
       alert("Bitte beachten sie, dass das Checkindatum vor dem Checkout liegt, und dass das Format yyyy-mm-dd ist!");
     }
